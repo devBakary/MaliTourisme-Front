6fd8279caecd1b7c4254309567550a2a41d245b8
bddiakite <ffffffffff>
2022-12-13 22:32:34 +0000

    travail au niveau de l'interface accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { InscrireComponent } from './inscrire/inscrire.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        redirectTo:"/login",
+        pathMatch: 'full'
+    },
+    {
+        path: "accueil",
+        component: AccueilComponent
+    },
+    {
+        path: "inscrire",
+        component: InscrireComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { InscrireComponent } from './inscrire/inscrire.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    InscrireComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
